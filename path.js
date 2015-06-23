313f7e6a10ebc6bc8730888da035f7ee59d24218
drewminns <drew.minns@me.com>
2015-06-23 12:11:26 -0400

    updated build

--- path.js
+++ path.js
@@ -10,44 +10,53 @@ api.home = function(req, res) {
 
 api.getListings = function (req, res) {
 
-	console.log(req.query.location);
+	if (api.data.length > 2) {
+		api.data.length = [];
+	}
 
-	var location = req.query.location.replace(/\s/g, '+'),
+	var location = req.query.location,
 			price = req.query.price,
 			rooms = req.query.rooms;
 
-	var clURL = 'http://toronto.craigslist.ca/search/apa?maxAsk=' + price + '&bedrooms=' + rooms + '&query=' + location + '#pic';
-	var kjURL = 'http://www.kijiji.ca/b-' + rooms + '-bedroom-apartments-condos/city-of-toronto/' + location + '/k0c212l1700273?price=__' + price;
+	if (location === undefined || price === undefined || rooms === undefined) {
+		res.json({ error: 'Parameters not included'});
+	} else {
 
+		var clURL = 'http://toronto.craigslist.ca/search/apa?maxAsk=' + price + '&bedrooms=' + rooms + '&query=' + location + '#pic';
+		var kjURL = 'http://www.kijiji.ca/b-' + rooms + '-bedroom-apartments-condos/city-of-toronto/' + location + '/k0c212l1700273?price=__' + price;
 
-	console.log(clURL);
-	function CLbuild(url) {
-		x(url, 'p.row', [{
-		  title: '.txt .hdrlnk',
-		  link: '.txt a.hdrlnk@href',
-		  posted: '.pl time@title',
-		  price: '.txt .price'
-		}])(function(err, obj) {
-		 api.data.push(obj)
-		});
+		function CLbuild(url) {
+			x(url, 'p.row', [{
+			  title: '.txt .hdrlnk',
+			  link: '.txt a.hdrlnk@href',
+			  posted: '.pl time@title',
+			  price: '.txt .price'
+			}])
+			(function(err, obj) {
+				if (err)
+					res.send(err);
+				 api.data.push(obj)
+			});
+		}
+
+		function KJbuild(url) {
+			x(url, '.regular-ad', [{
+				title: '.description a.title',
+				link: '.description a@href',
+				posted: '.posted',
+				price: '.price'
+			}])(function(err, obj) {
+			if (err)
+				res.send(err);
+			 api.data.push(obj)
+			});
+		}
+		CLbuild(clURL);
+		KJbuild(kjURL);
+		res.json(api.data);
+
 	}
 
-	function KJbuild(url) {
-		x(url, '.regular-ad', [{
-			title: '.description a.title',
-			link: '.description a@href',
-			posted: '.posted',
-			price: '.price'
-		}])(function(err, obj) {
-		 api.data.push(obj)
-		});
-	}
-
-	CLbuild(clURL);
-	KJbuild(kjURL);
-
-
-	res.json(api.data);
 }
 
 module.exports = api;
