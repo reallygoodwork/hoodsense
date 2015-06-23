984c1bc64bd1e0ededa290908cb5849b7f0609a4
drewminns <drew.minns@me.com>
2015-06-23 15:30:17 -0400

    jsonp bullllllshit

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ var express = require('express'),
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Credentials', true);
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  	res.header('Access-Control-Allow-Methods', 'GET');
+    next();
+});
+
 app.get('/', routes.home);
 app.get('/api', routes.getListings);
 
