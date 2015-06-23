73307bf5a5a488fe6be48759cb274ddc108446de
drewminns <drew.minns@me.com>
2015-06-23 15:40:44 -0400

    headers

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'http://45.55.140.101');
     res.header('Access-Control-Allow-Credentials', true);
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
   	res.header('Access-Control-Allow-Methods', 'GET');
